e9981485b767c9964e251e599f146234be34622d
Vats <nikhil-kumar.vats@capgemini.com>
2024-02-23 10:21:22 +0530

    add dependencies

---
+++ RedBus/src/test/java/Practice/RedBus/AppTest.java
@@ -0,0 +1,25 @@
+package Practice.RedBus;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AppTest{
+	public static void main(String[] args) throws Exception {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://www.redbus.in/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		//navigate to the train ticket page
+		driver.findElement(By.xpath("//span[text()='Train Tickets']")).click();
+	}
+}
---
+++ RedBus/src/test/java/Practice/RedBus/Standalone.java
@@ -0,0 +1,12 @@
+//package Practice.RedBus;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//
+//public class Standalone {
+//
+////	
+//
+//}
